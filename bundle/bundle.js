7d4e253bfd3563c8be834688405e269316b70873
Liangtian Zhang <lzhang40@binghamton.edu>
2018-09-30 20:51:38 -0400

    refactored fetching functions into util folder. main.js fetches instances and gunsales. initmap fetch map info now

--- bundle/bundle.js
+++ bundle/bundle.js
@@ -136,27 +136,15 @@ const handleMouseOut = (datum, i) => {
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__map__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__update_instances__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__listener_installer__ = __webpack_require__(5);
-
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__util_fetchData__ = __webpack_require__(6);
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  let instances;
-  const DOMElements = {};
-  Object(__WEBPACK_IMPORTED_MODULE_0__map__["a" /* default */])();
-
-  DOMElements.yearOptions = document.getElementById('year-options');
-  DOMElements.genderOptions = document.getElementById('gender-options');
-  DOMElements.venueOptions = document.getElementById('venue-options');
-  DOMElements.raceOptions = document.getElementById('race-options');
-  DOMElements.reset = document.getElementById('reset');
-
-  d3.csv("https://raw.githubusercontent.com/liamzhang40/nothing_political/master/csv/merge.csv").then(data => {
-    instances = data;
-    setTimeout(() => Object(__WEBPACK_IMPORTED_MODULE_1__update_instances__["a" /* default */])(data), 1000);
-    Object(__WEBPACK_IMPORTED_MODULE_2__listener_installer__["a" /* default */])(instances, DOMElements);
+  Object(__WEBPACK_IMPORTED_MODULE_1__util_fetchData__["a" /* fetchGunSales */])().then(gunSales => {
+    Object(__WEBPACK_IMPORTED_MODULE_1__util_fetchData__["b" /* fetchInstance */])().then(instances => {
+      Object(__WEBPACK_IMPORTED_MODULE_0__map__["a" /* default */])(gunSales, instances);
+    });
   });
 
   document.getElementsByClassName('modal-screen')[0].addEventListener('click', e => {
@@ -178,30 +166,23 @@ document.addEventListener('DOMContentLoaded', () => {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__map_style__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__update_chart__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__update_chart__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__update_instances__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__listener_installer__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__util_fetchData__ = __webpack_require__(6);
 
 
 
-let gunSales;
-let instances;
 
-const initMap = () => {
-  const svg = d3.select("svg");
-  const path = d3.geoPath(null);
 
-  d3.csv("https://raw.githubusercontent.com/liamzhang40/nothing_political/master/csv/nics_firearm_background_checks.csv").then(data => {
-    gunSales = data;
-  });
 
-  d3.csv("https://raw.githubusercontent.com/liamzhang40/nothing_political/master/csv/merge.csv").then(data => {
-    instances = data;
-  });
-
-  const mapPromise = d3.json("https://d3js.org/us-10m.v1.json").then(us => {
-    const usData = topojson.feature(us, us.objects.states).features;
-    d3.tsv("https://gist.githubusercontent.com/mbostock/4090846/raw/07e73f3c2d21558489604a0bc434b3a5cf41a867/us-state-names.tsv")
-    .then(stateNames => {
+const initMap = (gunSales, instances) => {
+  Object(__WEBPACK_IMPORTED_MODULE_3__util_fetchData__["c" /* fetchMap */])().then(us => {
+    Object(__WEBPACK_IMPORTED_MODULE_3__util_fetchData__["d" /* fetchStates */])().then(stateNames => {
+      
+      const usData = topojson.feature(us, us.objects.states).features;
+      const svg = d3.select("svg");
+      const path = d3.geoPath(null);
       const nameHash = {};
       stateNames.forEach(name => {
         nameHash[name.id] = name.name;
@@ -236,348 +217,44 @@ const initMap = () => {
         .attr("text-anchor", "middle")
         .attr("fill", "#6f9ba5")
         .on("click", handleClick);
+
+      mountInstances(instances);
       });
   });
 
+  const handleClick = datum => {
+    const state = d3.event.target.textContent;
+    const selectedYear = document.getElementById("year-options").value;
+    if (!selectedYear) {
+      window.alert("Please select a year!");
+      return;
+    } else if (selectedYear === 'old') {
+      window.alert("No background check records before 2014!");
+      return;
+    }
+  
+    const filteredGunSales = gunSales.filter(sale => {
+      return sale.month.slice(0,4) === selectedYear && sale.state === state;
+    });
+  
+    const filteredInstances = instances.filter(instance => {
+      return instance.date.slice(instance.date.length - 2) === selectedYear.slice(2) && instance.location === state;
+    });
+  
+    Object(__WEBPACK_IMPORTED_MODULE_0__update_chart__["a" /* default */])(filteredGunSales, filteredInstances);
+  };
+
+  const mountInstances = instances => {
+    Object(__WEBPACK_IMPORTED_MODULE_1__update_instances__["a" /* default */])(instances);
+    Object(__WEBPACK_IMPORTED_MODULE_2__listener_installer__["a" /* default */])(instances);
+  };
 };
-
-const handleClick = datum => {
-  const state = d3.event.target.textContent;
-  const selectedYear = document.getElementById("year-options").value;
-  if (!selectedYear) {
-    window.alert("Please select a year!");
-    return;
-  } else if (selectedYear === 'old') {
-    window.alert("No background check records before 2014!");
-    return;
-  }
-
-  const selectedGunSales = gunSales.filter(sale => {
-    return sale.month.slice(0,4) === selectedYear && sale.state === state;
-  });
-
-  const selectedInstances = instances.filter(instance => {
-    return instance.date.slice(instance.date.length - 2) === selectedYear.slice(2) && instance.location === state;
-  });
-
-  Object(__WEBPACK_IMPORTED_MODULE_1__update_chart__["a" /* default */])(selectedGunSales, selectedInstances);
-};
-
 
 /* harmony default export */ __webpack_exports__["a"] = (initMap);
 
 
 /***/ }),
-/* 3 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-const mapStyle = [
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#1d2c4d"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#8ec3b9"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1a3646"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.country",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#4b6878"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#64779e"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.neighborhood",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.province",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#4b6878"
-      }
-    ]
-  },
-  {
-    "featureType": "landscape.man_made",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#334e87"
-      }
-    ]
-  },
-  {
-    "featureType": "landscape.natural",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#023e58"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#283d6a"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#6f9ba5"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1d2c4d"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.business",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#023e58"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#3C7680"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#304a7d"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#98a5be"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1d2c4d"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#2c6675"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#255763"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#b0d5ce"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#023e58"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#98a5be"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1d2c4d"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#283d6a"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#3a4762"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#0e1626"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#4e6d70"
-      }
-    ]
-  }
-];
-
-/* unused harmony default export */ var _unused_webpack_default_export = (mapStyle);
-
-
-/***/ }),
+/* 3 */,
 /* 4 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -673,81 +350,82 @@ const parseInstances = instances => {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__update_instances__ = __webpack_require__(0);
 
 
-
-const listenerInstaller = (instances, DOMElements) => {
-  let selectorValues = {
-    yearOptions: "",
-    genderOptions: "",
-    venueOptions: "",
-    raceOptions: ""
+const listenerInstaller = (instances) => {
+  const selectorValues = {
+    "year-options": "",
+    "gender-options": "",
+    "venue-options": "",
+    "race-options": ""
   };
 
-  for (let key in DOMElements) {
-    if (DOMElements.hasOwnProperty(key)) {
-      if (key === "reset") {
-        DOMElements.reset.addEventListener('click', () => {
-          document.querySelectorAll("select").forEach(select => {
-            select.selectedIndex = 0;
-          });
-          selectorValues.yearOptions = "";
-          selectorValues.genderOptions = "";
-          selectorValues.venueOptions = "";
-          selectorValues.raceOptions = "";
-          Object(__WEBPACK_IMPORTED_MODULE_0__update_instances__["a" /* default */])(instances);
-        });
-      } else {
-        DOMElements[key].addEventListener('change', e => {
-          if (key === "yearOptions") selectorValues[key] = e.currentTarget.value.slice(2);
-          else selectorValues[key] = e.currentTarget.value;
-          console.log(selectorValues)
-          const filteredInstances = filterInstances(instances);
-          Object(__WEBPACK_IMPORTED_MODULE_0__update_instances__["a" /* default */])(filteredInstances);
-        });
+  document.getElementById('reset').addEventListener('click', () => {
+    document.querySelectorAll("select").forEach(select => {
+      select.selectedIndex = 0;
+    });
+    for (let option in selectorValues) {
+      if (selectorValues.hasOwnProperty(option)) {
+        selectorValues[option] = "";
       }
     }
-  }
+    Object(__WEBPACK_IMPORTED_MODULE_0__update_instances__["a" /* default */])(instances);
+  });
 
-  // DOMElements.year_options.addEventListener('change', e => {
-  //   year = e.currentTarget.value.slice(2);
-  //   const filteredInstances = filterInstances(instances);
-  //   updateInstances(filteredInstances);
-  // });
-
-  // DOMElements.venue_options.addEventListener('change', e => {
-  //   venue = e.currentTarget.value;
-  //   const filteredInstances = filterInstances(instances);
-  //   updateInstances(filteredInstances);
-  // });
-
-  // DOMElements.gender_options.addEventListener('change', e => {
-  //   gender = e.currentTarget.value;
-  //   const filteredInstances = filterInstances(instances);
-  //   updateInstances(filteredInstances);
-  // });
-
-  // DOMElements.race_options.addEventListener('change', e => {
-  //   race = e.currentTarget.value;
-  //   const filteredInstances = filterInstances(instances);
-  //   updateInstances(filteredInstances);
-  // });
-
+  document.querySelectorAll('select').forEach(selector => {
+    selector.addEventListener('change', e => {
+      selectorValues[selector.className] = e.currentTarget.value;
+      const filteredInstances = filterInstances(instances);
+      Object(__WEBPACK_IMPORTED_MODULE_0__update_instances__["a" /* default */])(filteredInstances);
+    });
+  });
+  
   const filterInstances = (instances) => {
+    console.log(selectorValues)
     const res = instances.filter(instance => {
       let date = instance.date;
       date = date.slice(date.length - 2);
   
-      return (!selectorValues.yearOptions || date === selectorValues.yearOptions || (parseInt(date) < 14 && selectorValues.yearOptions === "d")) &&
-        (!selectorValues.venueOptions || instance.venue === selectorValues.venueOptions) &&
-        (!selectorValues.genderOptions || instance.gender === selectorValues.genderOptions) &&
-        (!selectorValues.raceOptions || instance.race.toUpperCase() === selectorValues.raceOptions.toUpperCase());
+      return (!selectorValues["year-options"] || date === selectorValues["year-options"] || (parseInt(date) < 14 && selectorValues["year-options"] === "d")) &&
+        (!selectorValues["venue-options"] || instance.venue === selectorValues["venue-options"]) &&
+        (!selectorValues["gender-options"] || instance.gender === selectorValues["gender-options"]) &&
+        (!selectorValues["race-options"] || instance.race.toUpperCase() === selectorValues["race-options"].toUpperCase());
     });
   
     return res;
   };
 };
 
-
 /* harmony default export */ __webpack_exports__["a"] = (listenerInstaller);
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const fetchMap = () => (
+    d3.json("https://d3js.org/us-10m.v1.json")
+);
+/* harmony export (immutable) */ __webpack_exports__["c"] = fetchMap;
+
+
+const fetchStates = () => (
+    d3.tsv("https://gist.githubusercontent.com/mbostock/4090846/raw/07e73f3c2d21558489604a0bc434b3a5cf41a867/us-state-names.tsv")
+);
+/* harmony export (immutable) */ __webpack_exports__["d"] = fetchStates;
+
+
+const fetchInstance = () => (
+    d3.csv("https://raw.githubusercontent.com/liamzhang40/nothing_political/master/csv/merge.csv")
+);
+/* harmony export (immutable) */ __webpack_exports__["b"] = fetchInstance;
+
+
+const fetchGunSales = () => (
+    d3.csv("https://raw.githubusercontent.com/liamzhang40/nothing_political/master/csv/nics_firearm_background_checks.csv")
+);
+/* harmony export (immutable) */ __webpack_exports__["a"] = fetchGunSales;
+
+
 
 
 /***/ })
